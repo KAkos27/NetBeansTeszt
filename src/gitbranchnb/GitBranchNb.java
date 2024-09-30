f8df52e381754aeb90a7f48882641a6ae9e77a4a
kosztolanyi.akos <kosztolanyi.akos@terem0123.szaleziakk.local>
2024-09-30 08:55:00 +0200

    void fv()

--- src/gitbranchnb/GitBranchNb.java
+++ src/gitbranchnb/GitBranchNb.java
@@ -3,10 +3,11 @@ package gitbranchnb;
 public class GitBranchNb {
 
     public static void main(String[] args) {
-        fv();
+        new GitBranchNb().fv();
     }
 
-    private static void fv() {
+    private void fv() {
+        System.out.println("Nem static fv");
     }
 
 }
